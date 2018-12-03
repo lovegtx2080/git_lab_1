92822d3897e22a76bcf0854be801bbd7b94095e1
Britvin Egor <nvidiagtx660@mail.ru>
2018-12-03 10:17:58 +0400

    добавили умножение

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <conio.h>
 #include <math.h>
 using namespace std;
-
+//учебная
 int main()
 {
     int a, b;
@@ -13,5 +13,7 @@ int main()
 	cout << "Введите b: ";
 	cin >> b;
     cout << "Сумма a и b = " << a + b << endl;
+    cout << "Разность  a и b = " << a - b << endl;
+    cout << "Multi  a и b = " << a * b << endl;
 	return 0;
 }
